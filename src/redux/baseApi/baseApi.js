8fbdbb44ca44cfe3adf9d80fd07c722ee05c6e83
Aminulhoque01 <aminuhoque53@gmail.com>
2025-12-07 01:38:08 +0600

    build

--- src/redux/baseApi/baseApi.js
+++ src/redux/baseApi/baseApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "pokemonApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1/notice",
+    baseUrl: "https://nebs-it-backend-om25.vercel.app/api/v1/notice",
     prepareHeaders: (headers, { getState }) => {
       // Retrieve the token from your store or local storage
       const token = getState().auth.token;
